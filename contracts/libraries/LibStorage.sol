1f31c2abd0d4ff39eb9a5f905e973ee969e95874
DonGuillotine <infect3dlab@gmail.com>
2024-10-30 00:00:16 +0100

    Feat: Implemented basic storage structure

---
+++ contracts/libraries/LibStorage.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library LibStorage {
+    bytes32 constant STORAGE_POSITION = keccak256("nft.lending.diamond.storage");
+
+    struct NFTDeposit {
+        address owner;
+        address collection;
+        uint256 tokenId;
+        uint256 depositTime;
+        bool isLocked;
+        uint256 activeLoanId;
+    }
+
+    struct Loan {
+        address borrower;
+        uint256 loanAmount;
+        uint256 interestAmount;
+        uint256 startTime;
+        uint256 endTime;
+        LoanStatus status;
+        NFTCollateral collateral;
+    }
+
+    struct NFTCollateral {
+        address collection;
+        uint256 tokenId;
+        uint256 valueAtLock;
+    }
+
+    enum LoanStatus { None, Active, Repaid, Defaulted }
+
+    struct NFTLendingStorage {
+        mapping(address => mapping(uint256 => NFTDeposit)) nftDeposits;
+        mapping(address => uint256[]) userDepositedNFTs;
+        
+        mapping(uint256 => Loan) loans;
+        uint256 loanCounter;
+        
+        mapping(address => uint256) lenderBalances;
+        uint256 totalETHDeposited;
+        
+        mapping(address => uint256) nftFloorPrices;
+        uint256 collateralRatio;
+        uint256 interestRate;
+        uint256 loanDuration;
+        bool paused;
+
+        address admin;
+    }
+
+    function diamondStorage() internal pure returns (NFTLendingStorage storage ds) {
+        bytes32 position = STORAGE_POSITION;
+        assembly {
+            ds.slot := position
+        }
+    }
+}
